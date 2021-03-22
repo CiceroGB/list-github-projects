e12cf6ded3a0c055b68a2e6fa7b9f3f56b67d245
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-03-22 19:22:43 -0300

    components and styles

---
+++ src/componentes/RepositoryItem/index.tsx
@@ -0,0 +1,21 @@
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
+
+
+export function RepositoryItem() {
+    return (
+
+        <section className="repository-list">
+            <h1>
+                Lista de Repositorios
+            </h1>
+
+        </section>
+    )
+
+}
